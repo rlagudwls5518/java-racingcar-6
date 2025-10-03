34a68369c6d726e5820ce93f78d574213d6c54b2
rlagudwls5518 <rlagudwls5518@naver.com>
2025-10-03 14:04:02 +0900

    first commit

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+
+
+        String[] carName = scanner.nextLine().split(",");
+
+        Car[] car = new Car[carName.length];
+
+
+
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,12 @@
 package racingcar;
 
 public class Car {
+    private String name;
+
+    public void Car(String name){
+        this.name = name;
+    }
+
+
 
 }
