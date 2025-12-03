75ee5843fcf4415fd799c0dc4eda2e3e73bb496f
gudwls5518 <eldosura@gmail.com>
2025-12-03 14:21:16 +0900

    feat: 최종코테연습 first commit

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,13 +1,7 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameController.run();
-
-
-
     }
 }
